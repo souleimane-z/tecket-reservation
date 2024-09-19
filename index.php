15160a3bb3e3f9f966128ddb890340d4625bb11a
souleimane-z <souleimane.zeggai@gmail.com>
2024-09-19 15:48:30 +0200

    meta updated

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
 <html lang="fr">
 
 <head>
+<link rel="apple-touch-icon" sizes="180x180" href="./asset/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./asset/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./asset/favicon/favicon-16x16.png">
+<link rel="manifest" href="./asset/favicon/site.webmanifest">
+<link rel="mask-icon" href="./asset/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="./asset/favicon/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="./asset/favicon/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les Clefs de la Ville</title>
     <link rel="stylesheet" href="css/styles.css">
+    
 </head>
 
 <body>
